6c9bacb6524b355b9c5468f372c9d304656d184b
Cristian Alvarado <105570257+cut-the-freakin-steak@users.noreply.github.com>
2025-02-24 01:18:48 -0700

    for real this time
    on jod

---
+++ lua/plugins/material.lua
@@ -0,0 +1,8 @@
+return {
+    "marko-cerovac/material.nvim",
+    config = function()
+        -- set up color theme
+        -- i'm using material deep ocean for now
+        vim.g.material_style = "deep ocean"
+    end
+}
---
+++ lua/plugins/ultimate-autopair.lua
@@ -0,0 +1,8 @@
+return {
+	"altermo/ultimate-autopair.nvim",
+	event = { "InsertEnter", "CmdlineEnter" },
+	branch = "v0.6", -- recommended as each new version will have breaking changes
+	config = {
+		-- just calling so that we dont have to require() in init.lua
+	},
+}
