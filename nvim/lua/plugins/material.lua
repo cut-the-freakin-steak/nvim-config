c3e6df6c8d48280d36b4eebb1484d14d62b43bf6
Cristian Alvarado <105570257+cut-the-freakin-steak@users.noreply.github.com>
2025-02-24 01:14:51 -0700

    Delete nvim directory

--- nvim/lua/plugins/material.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "marko-cerovac/material.nvim",
-    config = function()
-        -- set up color theme
-        -- i'm using material deep ocean for now
-        vim.g.material_style = "deep ocean"
-    end
-}
