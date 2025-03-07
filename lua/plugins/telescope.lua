7d4944ddc7a02de712e49970919bbb6fd50b6571
cut-the-freakin-steak <calvaradolpez7@gmail.com>
2025-03-06 23:15:21 -0700

    added telescope-cmdline.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,11 +2,37 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
+		dependencies = {
+            "nvim-lua/plenary.nvim",
+            "jonarrien/telescope-cmdline.nvim",
+        },
+        opts = {
+            extensions = {
+                cmdline = {
+                    -- Adjust telescope picker size and layout
+                    picker = {
+                        layout_config = {
+                            width  = 60,
+                            height = 15,
+                        }
+                    },
+                    -- Adjust your mappings 
+                    mappings = {
+                        complete      = '<Tab>',
+                        run_selection = '<CR>',
+                        run_input     = '<CR>',
+                    },
+                },
+            },
+        },
+		config = function(_, opts)
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+            require("telescope").setup(opts)
+            require("telescope").load_extension("cmdline")
+            vim.keymap.set("n", ":", "<cmd>Telescope cmdline<cr>")
 		end,
 	},
 	{
